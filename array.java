2018053c564fc58ff6591a7db614d3b970d254cd
Negbwei <130391089+Negbwei@users.noreply.github.com>
2024-05-31 15:37:54 +0100

    Add files via upload

---
+++ array.java
@@ -0,0 +1,19 @@
+public class array {
+
+    public static void main(String[] args){
+        int[] numberarray = new int[5];
+        numberarray[0] =17;
+        numberarray[1] = 34;
+        numberarray[2] = 54;
+        numberarray[3] = 77;
+        numberarray[4] = 47;
+
+
+        for (int i=0; i< numberarray.length;i++){
+            //system.out.printIn(numberarray)
+            if (numberarray[1] ==54){
+        }
+        System.out.println("Found"+numberarray[i]);
+            break;
+    }
+}
